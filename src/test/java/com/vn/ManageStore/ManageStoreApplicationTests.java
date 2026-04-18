2a0e0081ced0edd84aa88a66fdd46098fae7cc6d
Thanhzz <nguyenthanh3557@gmail.com>
2026-04-18 15:02:44 +0700

    first commit

---
+++ src/test/java/com/vn/ManageStore/ManageStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.ManageStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
